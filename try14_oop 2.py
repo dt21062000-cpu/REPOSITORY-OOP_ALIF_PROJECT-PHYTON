865aa438cb5e6879a5889e6275003ae433f4f722
M alif rizqulloh <dt21062000@gmail.com>
2026-05-20 10:55:54 +0700

    Add files via upload

---
+++ try14_oop 2.py
@@ -0,0 +1,6 @@
+daya = 7
+while daya < 100:
+  print("Mengisi daya------ posisi:",daya, "%")
+  daya += 5 #Menambahkan daya setiap putaran
+ 
+print("Baterai Penuh")
---
+++ try7_oop 2.py
@@ -0,0 +1,24 @@
+a=1
+b=0
+c=1
+d=0
+
+print(int (not a))
+print(int (not b))
+print(int (not c))
+print(int (not d))
+print("\n")
+print(int (a or b))
+print(int (d or c))
+print(int (b or a))
+print(int (c or b))
+print("\n")
+print(int (a and b))
+print(int (c and d))
+print(int (a and c))
+print(int (b and d))
+print("\n")
+print(a^b)
+print(d^c)
+print(a^c)
+print(b^d)
